97d04781a0561a7e8f707add673ee8e1feefbbce
NMThang15 <155221171+NMThang15@users.noreply.github.com>
2025-03-12 10:18:03 +0700

    moneyb3

---
+++ doitien/Program.cs
@@ -0,0 +1,16 @@
+﻿//[Bài tập] Ứng dụng chuyển đổi tiền tệ  23000 VND/USD
+using System;
+
+namespace doitien
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int usd;
+            Console.WriteLine("Nhap USD: ");
+            usd = int.Parse(Console.ReadLine());
+            Console.WriteLine("=> VND: "+ (usd * 23000));
+        }
+    }
+}

dae8eaf19beb0df56241b2f9cc8a5310510eb3e8
NMThang15 <155221171+NMThang15@users.noreply.github.com>
2025-03-12 08:40:30 +0700

    datetime

---
+++ Datetime/Program.cs
@@ -0,0 +1,14 @@
+﻿//[Thực hành] Hiển thị thời gian của hệ thống
+using System;
+
+namespace Systemtime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("Datetime now is : " + localDate);
+        }
+    }
+}

f361119b089a4ccc316372e009068a0b9ff230a4
NMThang15 <155221171+NMThang15@users.noreply.github.com>
2025-03-12 10:02:26 +0700

    loichaob3

---
+++ loichao/Program.cs
@@ -0,0 +1,15 @@
+﻿//[Bài tập] Hiển thị lời chào
+using System;
+
+namespace loichao
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string yourname = Console.ReadLine();
+            Console.WriteLine("Heloooo " + yourname + " beautiful =.))) ");
+        }
+    }
+}

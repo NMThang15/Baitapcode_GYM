f0a7643b7930cb7200fdf4b29c747f0dfa93af6e
NMThang15 <155221171+NMThang15@users.noreply.github.com>
2025-03-12 09:24:38 +0700

    Dien_tich_HCN

---
+++ Dien_tich_HCN/Program.cs
@@ -0,0 +1,19 @@
+﻿//[Thực hành] Sử dụng toán tử
+using System;
+
+namespace dientich
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            float width;
+            float height;
+            Console.Write("Enter Width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Enter Height: ");
+            height = float.Parse(Console.ReadLine());
+            Console.WriteLine("Area is: " + (width * height));
+        }
+    }
+}

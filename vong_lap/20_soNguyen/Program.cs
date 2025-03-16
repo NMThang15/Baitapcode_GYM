d3436f1c1fa998854369738062227e1028f5d850
NMThang15 <155221171+NMThang15@users.noreply.github.com>
2025-03-16 23:29:06 +0700

    bt

---
+++ vong_lap/20_soNguyen/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace n_so_nguyen_to_dau
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 20;
+            HienThiKq(n);
+
+        }
+        static void HienThiKq(int n)
+        {
+            int count = 0;
+            int i = 2;
+            while(count< n)
+            {
+                if(ktNgTo(i))
+                {
+                    count++;
+                    Console.Write(i + " ");
+                }
+                i++;
+            }
+        }
+        static bool ktNgTo(int n)
+        {
+            if (n < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+    }
+}

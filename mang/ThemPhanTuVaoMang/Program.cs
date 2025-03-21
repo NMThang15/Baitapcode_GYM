27f65d1b109597bbe6be8e19bee8894f87cb6284
NMThang15 <155221171+NMThang15@users.noreply.github.com>
2025-03-21 19:20:28 +0700

    bt

---
+++ mang/ThemPhanTuVaoMang/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ThemPhanTuVaoMang
+{
+
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                int[] num = new int[] { 10, 4, 6, 7, 8 };
+                foreach (int bf in num)
+                {
+                    Console.Write(bf + " ");
+                }
+                Console.Write("\nnhap X: ");
+                int x = int.Parse(Console.ReadLine());
+                num = new int[] { 10, 4, 6, x, 7, 8 };
+                foreach (int at in num)
+                {
+                    Console.Write(at + " ");
+                }
+                Console.ReadKey();
+
+
+
+            }
+
+        }
+}

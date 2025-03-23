a6022b93cda6252ec2a9f02c4cef3a9bafd9854e
NMThang15 <155221171+NMThang15@users.noreply.github.com>
2025-03-23 09:34:00 +0700

    bt

--- mang/ThemPhanTuVaoMang/Program.cs
+++ mang/ThemPhanTuVaoMang/Program.cs
@@ -10,18 +10,42 @@
                 {
                     Console.Write(bf + " ");
                 }
-                Console.Write("\nnhap X: ");
+
+                Console.Write("\nnhap x vao vi tri can chen: ");
                 int x = int.Parse(Console.ReadLine());
-                num = new int[] { 10, 4, 6, x, 7, 8 };
-                foreach (int at in num)
+                Console.Write("\nnhap vao vi tri index: ");
+                int  index= int.Parse(Console.ReadLine());
+
+                if (index<0 || index > num.Length)
+            {
+                Console.WriteLine("Loi ko chen vao dc!!!");
+
+            }
+                else
+            {
+                int[] NewNum = new int[num.Length + 1];
+
+                for (int i = 0 , j = 0; i < NewNum.Length; i++)
+                {
+                    if (i == index)
+                    {
+                        NewNum[i] = x;
+                    }
+                    else
+                    {
+                        NewNum[i] = num[j];
+                        j++;
+                    }
+                }
+                Console.WriteLine("man sau khi chen:");
+                foreach (int at in NewNum)
                 {
                     Console.Write(at + " ");
                 }
-                Console.ReadKey();
-
-
-
             }
+            Console.ReadKey();
+
+        }
 
         }
 }

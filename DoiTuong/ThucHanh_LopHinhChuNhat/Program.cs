7369a3b6c1fcea0527cb7b1713ac364913b2a68f
NMThang15 <155221171+NMThang15@users.noreply.github.com>
2025-03-26 12:07:13 +0700

    bt

---
+++ DoiTuong/ThucHanh_LopHinhChuNhat/Program.cs
@@ -0,0 +1,58 @@
+﻿//[Thực hành] Lớp hình chữ nhật
+
+using System.Drawing;
+
+namespace ThucHanh_LopHinhChuNhat
+{
+    //Bước 1: Tạo lớp Rectangle, khai báo các thuộc tính (properties),
+    //định nghĩa các phương thức khởi tạo (contructors).
+    public class HCN
+    {
+        double rong, cao;
+
+        public HCN() 
+        { }
+        public HCN(double rong, double cao)
+        {
+            this.rong = rong;
+            this.cao = cao;
+
+        }
+        //Bước 2: Định nghĩa các phương thức GetArea(), GetPerimeter(), Display()
+        public double GetDienTich()
+        {
+            return this.rong * this.cao;
+        }
+        public double GetChuVi()
+        {
+            return (this.rong + this.cao) * 2;
+        }
+        public string Display()
+        {
+            return "HCN{" + "rong = " + rong + ", cao = " + cao + "}";
+        }
+    }
+    
+     //Bước 3: Tạo class chứa phương thức main để thực thi chương trình
+     public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Bước 4: Khai báo các biến lưu kích thước hình chữ nhật và gán giá trị
+            Console.WriteLine("Nhap rong: ");
+            double rong = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap cao: ");
+            //Bước 5: Khởi tạo một đối tượng thuộc lớp Rectangle với phương thức khởi tạo chứa 2 tham số width, height
+            double cao = Double.Parse(Console.ReadLine());
+            HCN hcn = new HCN(rong, cao);
+            //Bước 6: Gọi các phương thức của lớp Rectangle thông qua đối tượng rectangle
+            //đã tạo để hiển thị các thông số của hình chữ nhật
+            Console.WriteLine("Hinh Chu nhat \n" + hcn.Display());
+            Console.WriteLine("Chu vi hinh chu nhat: " + hcn.GetChuVi());
+            Console.WriteLine("Dien tich hinh chu nhat: " + hcn.GetDienTich());
+        }
+    }
+    
+
+
+}
---
+++ DoiTuong/ThucHanh_XayDungClassAnimal/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+ [Thực hành] Xây dựng class Animal
+Mục tiêu
+Luyện tập định nghĩa lớp, tạo đối tượng, kế thừa sử dụng các phương thức của đối tượng và phạm vi truy cập.
+
+Điều kiện
+– Biết cách khai báo lớp (thuộc tính, phương thức)
+
+– Biết cách khởi tạo đối tượng
+
+– Biết cách sử dụng các phương thức của đối tượng
+ */
+
+using System.Reflection.Metadata;
+
+namespace ThucHanh_XayDungClassAnimal
+{
+
+    //Bước 1: Tạo class Animal, khai báo thuộc tính (properties) ,
+    //định nghĩa hàm khởi tạo (contructor),
+    //và hàm PrintInfo là 1 abtract method hàm này sẽ được lớp con triển khai (implement)
+    public abstract class Animal
+    {
+        protected string Weight { get; set; }
+        protected string Height { get; set; }
+        public Animal(string weight, string height)
+        {
+            this.Weight = weight;
+            this.Height = height;
+        }
+        public abstract void PrintInfo();
+    }
+    //Bước 2: Tạo class Cat kế thừa từ lớp Animal, khai báo thuộc tính,
+    //định nghĩa hàm khởi tạo, và override hàm PrintInfo
+    class Cat : Animal
+    {
+        private string Name { get; set; }
+        public Cat(string weight, string height, string name) : base(weight, height)
+        {
+            this.Name = name;
+        }
+        public override void PrintInfo()
+        {
+            Console.WriteLine("Weight : {0}" + '\n' + "Height: {1}" + '\n' + "Name: {2}", this.Weight, this.Height, this.Name);
+        }
+    }
+        //Bước 3 : Tạo class Program, Tại hàm Main khởi tạo đối tượng Cat
+        //và triệu gọi phương thức PrintInfo
+       class Progarm
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("20kg", "1.5", "kitty");
+            cat.PrintInfo();
+        }
+    }
+  
+}
